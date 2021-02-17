27de6783637d6e01fddfd31245f5101ad961a5e7
Mark <sipizork@gmail.com>
2021-02-17 17:47:29 +0100

    valami2

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/check', require('./routes/api/check'));
-app.use('/api/holiday', require('./routes/api/holiday'));
-app.use('/api/title', require('./routes/api/title'));
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/check', require('./routes/api/check'));
+// app.use('/api/holiday', require('./routes/api/holiday'));
+// app.use('/api/title', require('./routes/api/title'));
 
 app.get('/', (req, res) => {
   res.send('API Running');
