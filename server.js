12c7a975a8be44e352f3f9d3605df3cf37c6b308
Mark <sipizork@gmail.com>
2021-02-17 17:49:15 +0100

    valami3

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('API Running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
